f76e9ac55cc6cb98b9941bb4e72519f6f354b13e
Toine MARLANGE <marlange.toine@protonmail.com>
2023-09-06 19:26:45 +0400

    added notifications if reviews needed to be approved

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Feedback;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountController extends AbstractController
 {
+
+    
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+    
     #[Route('/compte', name: 'account')]
     public function index(): Response
     {
-        return $this->render('account/index.html.twig');
+        $feedback = $this->em->getRepository(Feedback::class);
+    
+        // Comptez les avis non approuvés
+        $feedbackCount = $feedback->count(['approved' => false]);
+    
+        return $this->render('account/index.html.twig', [
+            'feedbackCount' => $feedbackCount
+        ]);
     }
 }
